11a3a56fa7b4b9b8b47c1bde7d34f143008d4ed5
Sergey <myxxze@gmail.com>
2023-05-08 01:36:07 +0300

    RabbitMQ settings fix for Docker.

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const microservice = app.connectMicroservice({
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://localhost:5672'],
+      urls: ['amqp://rabbitmq:5672'],  // 'amqp://localhost:5672' if starting on localhost
       queue: 'toGenresMs',
       queueOptions: {
         durable: false,
