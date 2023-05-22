9bb6df4de214d7645009e7eb226ea75a7e43b3a7
Sergey Klimov <myxxze@gmail.com>
2023-05-23 01:47:32 +0300

    Removed port listening.

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,12 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: `.${process.env.NODE_ENV}.env`
+      envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATABASE_HOST,
-      port: Number(process.env.DATABASE_PORT),
+      port: +process.env.DATABASE_PORT,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
